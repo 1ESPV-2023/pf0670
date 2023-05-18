990cd20168186620f67524345f5e2411e5935251
alecarlojesus <profalexandre.jesus@fiap.com.br>
2023-05-18 20:15:36 -0300

    Refatoração dos elementos no js...

--- js/login.js
+++ js/login.js
@@ -1,17 +1,20 @@
 
 //Recuperando o input após ele receber o focus
 const inputUser = document.querySelector("input[type='text']");
+const inputPass = document.querySelector("input[name='txtSenha']");
+const labelUser = document.querySelector("label[for='idUsuario']");
+const labelPass = document.querySelector("label[for='idSenha']");
+
 //Adicionando o evento de focus no inputUser
 // inputUser.addEventListener("focus", ()=>{
-//     // inputUser.setAttribute("NOME DO ATRIBUTO", "VALOR DO ATRIBUTO");
-//     //Quando o input ganha o focus alteramos a cor de seu outline.
-//     inputUser.setAttribute("style","outline-color:#ff0000;");
-// } );
-
+    //     // inputUser.setAttribute("NOME DO ATRIBUTO", "VALOR DO ATRIBUTO");
+    //     //Quando o input ganha o focus alteramos a cor de seu outline.
+    //     inputUser.setAttribute("style","outline-color:#ff0000;");
+    // } );
+    
 inputUser.addEventListener("keyup", ()=>{
     //Enquanto o usuário digitar, vamos contar a quantidade de caractéres que existe no campo, enquanto a quantidade for menor que 5, manteremos o outline na cor vermelha, assim que a quantidade atingir 5 ou mais mudamos a cor para verde.
-    const labelUser = document.querySelector("label[for='idNm']");
-
+    
     if(inputUser.value.length < 5){
         inputUser.setAttribute("style","outline-color:#ff0000;");
         labelUser.setAttribute("style","color:#ff0000;");
@@ -26,7 +29,6 @@ inputUser.addEventListener("keyup", ()=>{
 const eyePass = document.querySelector(".fa-eye");
 
 eyePass.addEventListener("click", ()=>{
-    const inputPass = document.querySelector("input[name='txtPass']");
 
     if(inputPass.getAttribute("type") == "password"){
         inputPass.setAttribute("type","text");
@@ -35,3 +37,5 @@ eyePass.addEventListener("click", ()=>{
     }
     
 });
+
+

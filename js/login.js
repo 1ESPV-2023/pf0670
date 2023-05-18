1253aede34140403286c386b4693b71edee37e9f
alecarlojesus <profalexandre.jesus@fiap.com.br>
2023-05-18 20:54:52 -0300

    Validação form com obj[ ]/

--- js/login.js
+++ js/login.js
@@ -4,6 +4,7 @@ const inputUser = document.querySelector("input[type='text']");
 const inputPass = document.querySelector("input[name='txtSenha']");
 const labelUser = document.querySelector("label[for='idUsuario']");
 const labelPass = document.querySelector("label[for='idSenha']");
+const msgStatus = document.querySelector("#msg");
 
 //Adicionando o evento de focus no inputUser
 // inputUser.addEventListener("focus", ()=>{
@@ -38,4 +39,52 @@ eyePass.addEventListener("click", ()=>{
     
 });
 
+//====================================================
+
+//CRIANDO OBJETOS
+let usuario1 = {
+    nomeCompleto : "José das Couves",
+    nomeUsuario : "jose",
+    senhaUsuario: "123456"
+}
+
+let usuario2 = {
+    nomeCompleto : "Paulo Cabuloso",
+    nomeUsuario : "popo",
+    senhaUsuario: "123456"
+}
+
+let usuario3 = {
+    nomeCompleto : "Edulado Violao",
+    nomeUsuario : "dudu",
+    senhaUsuario: "123456"
+}
+
+//CRIANDO UMA LISTA DE OBJETOS
+let listaDeUsuarios = [];
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
+listaDeUsuarios.push(usuario3);
+
+addEventListener("click", (evt)=>{
+
+    if(evt.target.id == "btnSubmit"){
+
+        const usuarioLogado = {
+            nomeUsuarioLogado : inputUser.value,
+            senhaUsuarioLogado: inputPass.value
+        }
+
+        //CRIANDO UMA ITERAÇÃO PARA VALIDAR A LISTA DE OBJETOS COM O OBJETO DO USUÁRIO VALIDADO
+        listaDeUsuarios.forEach((usuario)=>{
+
+            if(usuarioLogado.nomeUsuarioLogado == usuario.nomeUsuario && usuarioLogado.senhaUsuarioLogado == usuario.senhaUsuario){
+                msgStatus.setAttribute("style","color:#00ff00");
+                msgStatus.innerHTML = `<span><strong>O usuário ${usuario.nomeCompleto} realizou o login com SUCESSO!!</strong></span>`
+            }
+
+        });
+
+    }
+});
 
